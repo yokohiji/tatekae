75b8ff7cfb64b04e5e5564a85f225916fda6f33a
yokohiji <153804747+yokohiji@users.noreply.github.com>
2026-08-02 11:27:31 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebaseコンソールで取得した設定値です。
+// この値は公開しても問題ありません(Firebaseはこの仕組みを前提に設計されています)。
+// アクセス制御は Realtime Database の「ルール」側で行います(README参照)。
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5Cxed0hZbGdJAZ-pFCWZV6H220kmFBwY",
+  authDomain: "tatekae-c53d5.firebaseapp.com",
+  databaseURL: "https://tatekae-c53d5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tatekae-c53d5",
+  storageBucket: "tatekae-c53d5.firebasestorage.app",
+  messagingSenderId: "64250801936",
+  appId: "1:64250801936:web:a246f878371b1420c97f95",
+};
